c3e649202ad48879bacc87bc0dbbe3b97a5970b0
Bogdanrg <bogdan20160902@gmail.com>
2023-07-21 12:57:55 +0300

    setup

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from dotenv import load_dotenv
 from fastapi import FastAPI
 
 from core.database import db
+from promotion_routes import promotion_router
 
 load_dotenv()
 
@@ -14,3 +15,6 @@ app = FastAPI(title=os.getenv("APP_TITLE"))
 async def hello() -> str:
     new_secret = await db["trading"].insert_one({"promotion": "Bitcoin"})
     return f"{new_secret.inserted_id}"
+
+
+app.include_router(promotion_router)
---
+++ promotion_routes.py
@@ -0,0 +1,3 @@
+from fastapi import APIRouter
+
+promotion_router = APIRouter(prefix="/promotions", tags=["promotions"])
