68f81eff62ea5e6a3f8ac7e2e9fc24ad84b0a12c
Bogdanrg <bogdan20160902@gmail.com>
2023-08-03 15:19:39 +0300

    ci

--- services/celery.py
+++ services/celery.py
@@ -31,5 +31,6 @@ def run_update_promotions() -> None:
 
 async def update_promotions() -> None:
     promotions = await parse_promotions()
+    promotions['action'] = 'rate_update'
     await send_data(promotions)
     await PromotionRepository.update_one(promotions)
