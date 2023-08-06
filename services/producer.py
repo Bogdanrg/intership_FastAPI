4bda906d1c0c3d908a4d76a44706ac85374d5045
Bogdanrg <bogdan20160902@gmail.com>
2023-08-06 20:50:44 +0300

    tests

--- services/producer.py
+++ services/producer.py
@@ -1,17 +1,18 @@
+import asyncio
 import json
+import logging
 
 from aiokafka import AIOKafkaProducer
 
 from config import app_settings
 
 
+loop = asyncio.get_event_loop()
+producer = AIOKafkaProducer(loop=loop, bootstrap_servers=[app_settings.BOOTSTRAP_SERVER])
+
+
 async def send_data(data: dict) -> None:
-    producer = AIOKafkaProducer(bootstrap_servers=[app_settings.BOOTSTRAP_SERVER])
-    await producer.start()
-    try:
-        data = {"action": "rate_update", "result": [{"name": "USDC", "price": 0.8}]}
-        await producer.send_and_wait(
-            app_settings.KAFKA_TOPIC_NAME, json.dumps(data).encode("utf-8")
-        )
-    finally:
-        await producer.stop()
+    await producer.send_and_wait(
+        app_settings.KAFKA_TOPIC_NAME, json.dumps(data).encode("utf-8")
+    )
+    logging.info(data)
--- tests/test_routers.py
+++ tests/test_routers.py
@@ -1,12 +1,13 @@
 import pytest
 from httpx import AsyncClient
+from config import app_settings
 
 from main import app
 
 
 @pytest.mark.asyncio
 async def test_pull() -> None:
-    async with AsyncClient(app=app, base_url="http://127.0.0.1:8080") as ac:
+    async with AsyncClient(app=app, base_url=app_settings.BASE_URL) as ac:
         response = await ac.get("/api/v1/promotions/pull")
     assert response.status_code == 200
     assert set(response.json()[0].keys()) == {"_id", "name", "price"}
