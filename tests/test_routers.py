f891ae6745cc1863486c247066ffe0e7a4458649
Bogdanrg <bogdan20160902@gmail.com>
2023-08-07 12:52:47 +0300

    tests

--- tests/test_routers.py
+++ tests/test_routers.py
@@ -3,10 +3,12 @@ from httpx import AsyncClient
 
 from config import app_settings
 from main import app
+from services.producer import producer
 
 
 @pytest.mark.asyncio
 async def test_pull() -> None:
+    await producer.start()
     async with AsyncClient(app=app, base_url=app_settings.BASE_URL) as ac:
         response = await ac.get("/api/v1/promotions/pull")
     assert response.status_code == 200
